8ebb6b5dd12dc987a14728d29b293eadd25f3aaf
Vikram Jamkar <vikram.jamkar2275@gmail.com>
2026-06-07 20:40:46 +0530

    Remove Quiz App from root

--- src/main/java/com/quiz/quizapp/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quiz.quizapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.quiz.quizapp.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin,Integer>{
-
-}
--- src/main/java/com/quiz/quizapp/repository/SubjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quiz.quizapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.quiz.quizapp.entity.Subject;
-
-public interface SubjectRepository extends JpaRepository<Subject, Integer>{
-
-}
